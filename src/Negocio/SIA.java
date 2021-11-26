81b43ff88ff4eb511518490d1994c2b4295a4742
JuanPCT <JuanPCT@DESKTOP-558C5I8>
2021-11-26 09:05:03 -0500

    Tercera Actualizacion

--- src/Negocio/SIA.java
+++ src/Negocio/SIA.java
@@ -40,19 +40,36 @@ public class SIA {
         Object datos[] = archivo.leerArchivo();
         //i=1 , por que la cabecera NO se procesa
         //codigos;nombre de estudiante;email;semestre
+        String datoMaterias = datos[0].toString();
+        String datos2[] = datoMaterias.split(",");
+        String nombresMaterias[] = new String[9];
+        for (int i = 4; i < datos.length; i++) {
+            nombresMaterias[i-4]=datos2[i];
+        }
+        
         for (int i = 1; i < datos.length; i++) {
             String datoEstudiante = datos[i].toString();
-            String datos2[] = datoEstudiante.split(",");
+            String datosestudiantes[] = datoEstudiante.split(",");
             int codigo = Integer.parseInt(datos2[0]);
             String nombre = datos2[1];
             boolean sexo = Boolean.parseBoolean(datos2[2]);
             short grado = Short.parseShort(datos2[3]);
-            //matematica,sociales,ingles,educación_física,artes,ciencias,lenguaje,fisica,quimica
-            Materia matematica = new Materia("matematica",datos2[4].charAt(0));
-            //Esto para coincidir con el índice del vector
-            grado--;
+            
             //creo un objeto estudiante con los datos obtenidos del csv
             Estudiante estudiante=new Estudiante(codigo, nombre, sexo);
+            
+            //Guardo los nombres de las materias matematica,sociales,ingles,educación_física,artes,ciencias,lenguaje,fisica,quimica
+            for (int j = 0; j < nombresMaterias.length; j++) {
+                if(!datos2[j+4].equals("-")){
+                    Materia materia = new Materia(nombresMaterias[i],datos2[j+4].charAt(0));
+                    estudiante.addMateria(materia);
+                } 
+                
+            }
+            
+            //Resto para coincidir con el índice del vector
+            grado--;
+            
             //Donde insertar ese estudiante?
             this.grados[grado].addEstudiante(estudiante);
             //System.out.println(fila.toString());
